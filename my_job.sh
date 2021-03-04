623405ee9b067afff165518b861c2b10dd775a6e
x93ma@edu.uwaterloo.ca <x93ma@edu.uwaterloo.ca>
2021-03-04 10:21:46 -0500

    dpr projection

--- my_job.sh
+++ my_job.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --mem=0
+#SBATCH --mem=64
 #SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --time=48:0:0
+#SBATCH --output=logs/%j.out
 export CUDA_AVAILABLE_DEVICES=0,1,2,3
 
+source ~/.bashrc
 conda activate farm
-python examples/dpr_encoder.py
-
+python examples/dpr_encoder_projection.py
